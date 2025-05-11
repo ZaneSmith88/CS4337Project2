eaee44f3d754b0fa685008a0df0c13107066e41d
ZaneSmith88 <149336407+ZaneSmith88@users.noreply.github.com>
2025-05-11 11:02:53 -0500

    Update Project2.pl 3

--- Project2.pl
+++ Project2.pl
@@ -12,18 +12,37 @@ NOTE: Atoms or symbols are always lowercase, variables are uppercase.
 main:-
     test.
     
+% Input
 get_input_path(InputPath) :-
     format("Enter a LIST of directions:~n"),
     read(InputPath).
     
     
+% Our move predicates
+move(up,    (Row,Col), (Row2,Col)) :- 
+    Row2 is Row - 1.
+move(down,  (Row,Col), (Row2,Col)) :- 
+    Row2 is Row + 1.
+move(left,  (Row,Col), (Row,Col2)) :- 
+    Col2 is Col - 1.
+move(right, (Row,Col), (Row,Col2)) :- 
+    Col2 is Col + 1.
+    
+
+% A simple get cell function
+cell(Maze, (Row, Col), CurrCell) :-
+    nth0(Row, Maze, CurrRow),
+    nth0(Col, CurrRow, CurrCell).
+    
+    
+% Gets the cell and checks if it's a wall.
 valid_move(Maze, (Row, Col)) :-
     %nth0 to just grab at index.
-    nth0(Row, Maze, CurrRow),
-    nth0(Col, CurrRow, CurrCell),
+    cell(Maze, (Row, Col), CurrCell),
     CurrCell \= w. % Effectively a boolean, returns if CurrCell ISNT a wall.
     
     
+% If statement that prints based on the output of valid_move
 test_valid_move(Maze, Position) :-
     (valid_move(Maze, Position) % if
         ->  
@@ -31,10 +50,29 @@ test_valid_move(Maze, Position) :-
         ;   
             format("~w is NOT valid.~n", [Position])
     ).
+    
+
+% A bit like java's overloading vvv
+iterate([], Maze, Pos) :-
+    format("End of instructions.~n"),
+    cell(Maze, Pos, LastCellWeReached),
+    (LastCellWeReached == e % if
+        ->  
+            format("Win!")
+        ;   
+            format("Lose!")
+    ).
+
+
+% Iteration recursion
+iterate([FirstInstruction|RestInstructions], Maze, CurrPos) :-
+    move(FirstInstruction, CurrPos, NextPos),
+    valid_move(Maze, NextPos), 
+    iterate(RestInstructions, Maze, NextPos). % Like Racket with the Cons/Rest structures
+    % NextPos becomes CurrPos...
 
 
 test :-
-    
     % NOTE: Commas separate goals until the query is closed with a period.
     
     Maze = [
@@ -44,12 +82,9 @@ test :-
         [w, e, w, w]
     ],
     
-    Position = (2, 2),
     get_input_path(InputPath),
     format("You entered: ~w~n", [InputPath]),
-    
-    % If else block ()->;
-    test_valid_move(Maze, Position).
+    iterate(InputPath, Maze, (1,2)).
     
     % "." End of query
     
